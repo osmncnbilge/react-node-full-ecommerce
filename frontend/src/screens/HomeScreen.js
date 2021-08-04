42e390420b23b609aa251ad1776bcc0b0f42316e
osmncnbilge <osmn.cn87@hotmail.com>
2021-08-04 19:26:48 +0300

    create product details

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import data from "../data";
+
+function HomeScreen(props) {
+  return (
+    <div>
+      <ul className="products">
+        {data.products.map((product) => (
+          <li key={product._id}>
+            <div className="product">
+              <Link to={`/product/${product._id}`}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </Link>
+              <div className="product-name">
+                <Link to={`/product/${product._id}`}>{product.name}</Link>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">
+                {product.rating} Start ({product.numReviews})
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default HomeScreen;
